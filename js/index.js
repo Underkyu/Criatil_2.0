fc95bd407a3b24ee4cf2002d0d0b23bf2d04d6f3
Miguel Yudi <miguel.yudi.baba@gmail.com>
2024-07-25 00:51:05 -0300

    Fazendo commit do estado atual do index

---
+++ js/index.js
@@ -0,0 +1,62 @@
+//Iniciando carrossel
+var swiper = new Swiper(".carrossel", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    effect: "fade",
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+    },
+    lazyLoading: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }
+  });
+
+  //Iniciando product slider
+
+  var swiper2 = new Swiper(".product", {
+    slidesPerView: 3,
+    loop : true,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+325: {
+  slidesPerView: 2,
+  spaceBetween: 30,
+},
+640: {
+slidesPerView: 2.5,
+spaceBetween: 30,
+},
+768: {
+slidesPerView: 3,
+spaceBetween: 60,
+},
+1024: {
+  slidesPerView: 3.5,
+  spaceBetween: 60,
+},
+1280: {
+  slidesPerView: 4.1,
+  spaceBetween: 60,
+},
+},
+  });
+
+  var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });

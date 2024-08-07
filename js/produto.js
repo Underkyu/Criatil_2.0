7ad46d1876bce7d5a27abee84ef90ac4e6a0f274
Miguel Yudi <miguel.yudi.baba@gmail.com>
2024-08-07 00:51:03 -0300

    Finalizando página de produto (quase finalizado)

--- js/produto.js
+++ js/produto.js
@@ -12,3 +12,41 @@ function stepper(btn){
         myInput.setAttribute("value", newValue);
     }
 }
+
+  //Iniciando product slider
+
+  var swiper2 = new Swiper(".product", {
+    slidesPerView: 3,
+    loop : true,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".next-product",
+      prevEl: ".prev-product",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+325: {
+  slidesPerView: 2,
+  spaceBetween: 30,
+},
+640: {
+slidesPerView: 2.5,
+spaceBetween: 30,
+},
+768: {
+slidesPerView: 3,
+spaceBetween: 60,
+},
+1024: {
+  slidesPerView: 3.5,
+  spaceBetween: 60,
+},
+1280: {
+  slidesPerView: 4.1,
+  spaceBetween: 60,
+},
+},
+  });

ed31e48bf76be178843d365d808f6a1afbcd09b9
Miguel Yudi <miguel.yudi.baba@gmail.com>
2024-10-26 02:07:54 -0300

    Alterei a página de produto e passei as páginas de adm para o formato php

---
+++ VERSAO_PHP/html/avaliacoesGrnt.php
@@ -0,0 +1,389 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Avaliações</title>
+    <link rel="stylesheet" href="../css/avaliacoesGrnt.css" />
+  </head>
+  <body>
+    <div class="container">
+      <h1 class="titulo">Clientes Cadastrados</h1>
+      <!--Titulo em cima da caixa dos clientes-->
+
+      <div class="principal">
+        <div class="fundo">
+          <!--Fundo azul que fica atrás dos clientes-->
+
+          <div class="box_avaliacao">
+            <!--Div que contem as avaliacoes-->
+
+            <div class="avaliacao">
+              <!--Div que contem uma das avaliações-->
+              <!--Começo card avaliação-->
+              <div class="card_avaliacao">
+                <img
+                  src="../imagens/Avaliacoes/teto_perfil.jpg"
+                  alt="Foto de perfil"
+                  class="foto_perfil"
+                /><!--Foto de perifl da avalição-->
+                <div class="detalhes_avaliacoes">
+                  <!--Detalhes como nome do avaliador e a avaliação em si-->
+
+                  <div class="nome_avaliacao">
+                    <h5 class="nome">Kasane Teto</h5>
+                    <!--Nome da conta-->
+
+                    <!--Estrelas ao lado do nome-->
+                    <div class="estrelas">
+                      <img
+                        src="../imagens/Icons/estrela.png"
+                        alt="estrela"
+                        class="primeira_estrela"
+                      />
+                      <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                      <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                      <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                      <img
+                        src="../imagens/Icons/meia_estrela.png"
+                        alt="meia estrela"
+                      />
+                    </div>
+                  </div>
+
+                  <p class="titulo_avaliacao">Amei</p>
+                  <!--Titulo da avaliacao-->
+
+                  <p class="texto_avaliacao">
+                    Produto de otima qualidade, entrega rápida e é a hatsune
+                    miku \o/
+                  </p>
+                  <!--Texto da avaliacao-->
+                </div>
+              </div>
+              <!--Fim card avaliação-->
+
+              <div class="botao_deletar">
+                <button class="deletar">Deletar</button>
+              </div>
+            </div>
+
+            <div class="avaliacao">
+                <!--Div que contem uma das avaliações-->
+                <!--Começo card avaliação-->
+                <div class="card_avaliacao">
+                  <img
+                    src="../imagens/Avaliacoes/teto_perfil.jpg"
+                    alt="Foto de perfil"
+                    class="foto_perfil"
+                  /><!--Foto de perifl da avalição-->
+                  <div class="detalhes_avaliacoes">
+                    <!--Detalhes como nome do avaliador e a avaliação em si-->
+  
+                    <div class="nome_avaliacao">
+                      <h5 class="nome">Kasane Teto</h5>
+                      <!--Nome da conta-->
+  
+                      <!--Estrelas ao lado do nome-->
+                      <div class="estrelas">
+                        <img
+                          src="../imagens/Icons/estrela.png"
+                          alt="estrela"
+                          class="primeira_estrela"
+                        />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img
+                          src="../imagens/Icons/meia_estrela.png"
+                          alt="meia estrela"
+                        />
+                      </div>
+                    </div>
+  
+                    <p class="titulo_avaliacao">Amei</p>
+                    <!--Titulo da avaliacao-->
+  
+                    <p class="texto_avaliacao">
+                      Produto de otima qualidade, entrega rápida e é a hatsune
+                      miku \o/
+                    </p>
+                    <!--Texto da avaliacao-->
+                  </div>
+                </div>
+                <!--Fim card avaliação-->
+  
+                <div class="botao_deletar">
+                  <button class="deletar">Deletar</button>
+                </div>
+              </div>
+
+              <div class="avaliacao">
+                <!--Div que contem uma das avaliações-->
+                <!--Começo card avaliação-->
+                <div class="card_avaliacao">
+                  <img
+                    src="../imagens/Avaliacoes/teto_perfil.jpg"
+                    alt="Foto de perfil"
+                    class="foto_perfil"
+                  /><!--Foto de perifl da avalição-->
+                  <div class="detalhes_avaliacoes">
+                    <!--Detalhes como nome do avaliador e a avaliação em si-->
+  
+                    <div class="nome_avaliacao">
+                      <h5 class="nome">Kasane Teto</h5>
+                      <!--Nome da conta-->
+  
+                      <!--Estrelas ao lado do nome-->
+                      <div class="estrelas">
+                        <img
+                          src="../imagens/Icons/estrela.png"
+                          alt="estrela"
+                          class="primeira_estrela"
+                        />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img
+                          src="../imagens/Icons/meia_estrela.png"
+                          alt="meia estrela"
+                        />
+                      </div>
+                    </div>
+  
+                    <p class="titulo_avaliacao">Amei</p>
+                    <!--Titulo da avaliacao-->
+  
+                    <p class="texto_avaliacao">
+                      Produto de otima qualidade, entrega rápida e é a hatsune
+                      miku \o/
+                    </p>
+                    <!--Texto da avaliacao-->
+                  </div>
+                </div>
+                <!--Fim card avaliação-->
+  
+                <div class="botao_deletar">
+                  <button class="deletar">Deletar</button>
+                </div>
+              </div>
+
+              <div class="avaliacao">
+                <!--Div que contem uma das avaliações-->
+                <!--Começo card avaliação-->
+                <div class="card_avaliacao">
+                  <img
+                    src="../imagens/Avaliacoes/teto_perfil.jpg"
+                    alt="Foto de perfil"
+                    class="foto_perfil"
+                  /><!--Foto de perifl da avalição-->
+                  <div class="detalhes_avaliacoes">
+                    <!--Detalhes como nome do avaliador e a avaliação em si-->
+  
+                    <div class="nome_avaliacao">
+                      <h5 class="nome">Kasane Teto</h5>
+                      <!--Nome da conta-->
+  
+                      <!--Estrelas ao lado do nome-->
+                      <div class="estrelas">
+                        <img
+                          src="../imagens/Icons/estrela.png"
+                          alt="estrela"
+                          class="primeira_estrela"
+                        />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img
+                          src="../imagens/Icons/meia_estrela.png"
+                          alt="meia estrela"
+                        />
+                      </div>
+                    </div>
+  
+                    <p class="titulo_avaliacao">Amei</p>
+                    <!--Titulo da avaliacao-->
+  
+                    <p class="texto_avaliacao">
+                      Produto de otima qualidade, entrega rápida e é a hatsune
+                      miku \o/
+                    </p>
+                    <!--Texto da avaliacao-->
+                  </div>
+                </div>
+                <!--Fim card avaliação-->
+  
+                <div class="botao_deletar">
+                  <button class="deletar">Deletar</button>
+                </div>
+              </div>
+
+              <div class="avaliacao">
+                <!--Div que contem uma das avaliações-->
+                <!--Começo card avaliação-->
+                <div class="card_avaliacao">
+                  <img
+                    src="../imagens/Avaliacoes/teto_perfil.jpg"
+                    alt="Foto de perfil"
+                    class="foto_perfil"
+                  /><!--Foto de perifl da avalição-->
+                  <div class="detalhes_avaliacoes">
+                    <!--Detalhes como nome do avaliador e a avaliação em si-->
+  
+                    <div class="nome_avaliacao">
+                      <h5 class="nome">Kasane Teto</h5>
+                      <!--Nome da conta-->
+  
+                      <!--Estrelas ao lado do nome-->
+                      <div class="estrelas">
+                        <img
+                          src="../imagens/Icons/estrela.png"
+                          alt="estrela"
+                          class="primeira_estrela"
+                        />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img
+                          src="../imagens/Icons/meia_estrela.png"
+                          alt="meia estrela"
+                        />
+                      </div>
+                    </div>
+  
+                    <p class="titulo_avaliacao">Amei</p>
+                    <!--Titulo da avaliacao-->
+  
+                    <p class="texto_avaliacao">
+                      Produto de otima qualidade, entrega rápida e é a hatsune
+                      miku \o/
+                    </p>
+                    <!--Texto da avaliacao-->
+                  </div>
+                </div>
+                <!--Fim card avaliação-->
+  
+                <div class="botao_deletar">
+                  <button class="deletar">Deletar</button>
+                </div>
+              </div>
+
+              <div class="avaliacao">
+                <!--Div que contem uma das avaliações-->
+                <!--Começo card avaliação-->
+                <div class="card_avaliacao">
+                  <img
+                    src="../imagens/Avaliacoes/teto_perfil.jpg"
+                    alt="Foto de perfil"
+                    class="foto_perfil"
+                  /><!--Foto de perifl da avalição-->
+                  <div class="detalhes_avaliacoes">
+                    <!--Detalhes como nome do avaliador e a avaliação em si-->
+  
+                    <div class="nome_avaliacao">
+                      <h5 class="nome">Kasane Teto</h5>
+                      <!--Nome da conta-->
+  
+                      <!--Estrelas ao lado do nome-->
+                      <div class="estrelas">
+                        <img
+                          src="../imagens/Icons/estrela.png"
+                          alt="estrela"
+                          class="primeira_estrela"
+                        />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img
+                          src="../imagens/Icons/meia_estrela.png"
+                          alt="meia estrela"
+                        />
+                      </div>
+                    </div>
+  
+                    <p class="titulo_avaliacao">Amei</p>
+                    <!--Titulo da avaliacao-->
+  
+                    <p class="texto_avaliacao">
+                      Produto de otima qualidade, entrega rápida e é a hatsune
+                      miku \o/
+                    </p>
+                    <!--Texto da avaliacao-->
+                  </div>
+                </div>
+                <!--Fim card avaliação-->
+  
+                <div class="botao_deletar">
+                  <button class="deletar">Deletar</button>
+                </div>
+              </div>
+
+              <div class="avaliacao">
+                <!--Div que contem uma das avaliações-->
+                <!--Começo card avaliação-->
+                <div class="card_avaliacao">
+                  <img
+                    src="../imagens/Avaliacoes/teto_perfil.jpg"
+                    alt="Foto de perfil"
+                    class="foto_perfil"
+                  /><!--Foto de perifl da avalição-->
+                  <div class="detalhes_avaliacoes">
+                    <!--Detalhes como nome do avaliador e a avaliação em si-->
+  
+                    <div class="nome_avaliacao">
+                      <h5 class="nome">Kasane Teto</h5>
+                      <!--Nome da conta-->
+  
+                      <!--Estrelas ao lado do nome-->
+                      <div class="estrelas">
+                        <img
+                          src="../imagens/Icons/estrela.png"
+                          alt="estrela"
+                          class="primeira_estrela"
+                        />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img src="../imagens/Icons/estrela.png" alt="estrela" />
+                        <img
+                          src="../imagens/Icons/meia_estrela.png"
+                          alt="meia estrela"
+                        />
+                      </div>
+                    </div>
+  
+                    <p class="titulo_avaliacao">Amei</p>
+                    <!--Titulo da avaliacao-->
+  
+                    <p class="texto_avaliacao">
+                      Produto de otima qualidade, entrega rápida e é a hatsune
+                      miku \o/
+                    </p>
+                    <!--Texto da avaliacao-->
+                  </div>
+                </div>
+                <!--Fim card avaliação-->
+  
+                <div class="botao_deletar">
+                  <button class="deletar">Deletar</button>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div class="acoes">
+          <div class="pesquisar">
+            <input
+              type="text"
+              class="pesquisar"
+              placeholder="Pesquisar brinquedo"
+            />
+            <img
+              src="../imagens/Header/Lupa.png"
+              alt="Lupa"
+              class="pesquisar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>

6a3c934d864bc8d7665e049df1948d66cc32070d
CNSinelli <nicolassinelli1@gmail.com>
2024-11-09 19:45:16 -0300

    teste

--- VERSAO_PHP/html/selos&categoriasGrnt.php
+++ VERSAO_PHP/html/selos&categoriasGrnt.php
@@ -35,6 +35,7 @@ $seloDao = new SeloDAO($conn, $BASE_URL);
 
     <div class="container">
         <h1 class="titulo">Categorias & Selos</h1> <!--Titulo em cima da caixa dos brinquedos-->
+        <div class="boxes">
         <div class="fundo"> <!--Fundo azul que fica atrás dos brinquedos-->
             <div class="box_brinquedos" id="brinquedos-container"><!--Div que contem os brinquedos-->
 
@@ -43,22 +44,31 @@ $seloDao = new SeloDAO($conn, $BASE_URL);
                     <div class="titulo">Nome</div>
                 </div>
 
-                <!--começo da div que contém uma das categorias-->
 
                 <?php 
                     foreach ($categorias as $categoria) {
                 ?>
-
+                <!-- div que contém as categorias -->
                 <div class="brinquedo"> 
                     <p class="informacao"><?php echo $categoria['Codigo_Categoria']; ?></p> <!--Id-->
                     <p class="informacao"><?php echo $categoria['Nome_Categoria']; ?></p> <!--Nome-->
                 </div>
-
                 <?php } ?>
 
-                <!--fim da div que contém uma das categorias-->
+                <?php 
+                    foreach ($selos as $selo) {
+                ?>
+                <!-- div que contém os selos -->
+                <div class="brinquedo"> 
+                    <p class="informacao"><?php echo $selo['Codigo_Selo']; ?></p> <!--Id-->
+                    <p class="informacao"><?php echo $selo['Tipo']; ?></p> <!--Nome-->
+                </div>
+                <?php } ?>
+
+
                 
             </div>
+        </div>
         </div>
         <div class="acoes">
             <button class="adicionar" id="btnAdicionar">Adicionar categoria</button>
@@ -68,152 +78,24 @@ $seloDao = new SeloDAO($conn, $BASE_URL);
         </div>
     </div>
 
-<!-- form de adicionar categorias -->
+<!-- form de adicionar  -->
 <div id="form-container1" class="formInsert">
     <form method="POST" id="formInsert-Brinquedo" class="formInsert-Brinquedo" action="../controller/produtoProcess.php">
     <h2>Adicionar Categoria</h2>
     <div class="div-q-separa-socorro">
     <div class="form-div"><!-- div q contém as inputs -->
-        
         <input type="text" name="Descricao" placeholder="Descrição" required>
         
         <input type="text" name="Faixa_Etaria" placeholder="Faixa Etária" required>
     </div>
-    <div class="form-div-img">
-    <div class="imagens-container">
-        <div class="imagem-input">
-            <label>Imagem Principal:</label>
-            <input type="text" id="inserirImagem1" name="Imagem1" placeholder="Caminho da Imagem" required>
-            <div class="preview-div" id="inserirPreviewdiv1">
-                <label>Preview da imagem:</label>
-                <img id="inserirPreview1" src="" class="imagemPreview">
-            </div>
-            <input type="hidden" name="numImagem1" value="1">
-        </div>
-        
-        <div class="imagem-input">
-            <label>Imagem 2 (opcional):</label>
-            <input type="text" id="inserirImagem2" name="Imagem2" placeholder="Caminho da Imagem">
-            <div class="preview-div" id="inserirPreviewdiv2">
-                <label>Preview da imagem:</label>
-                <img id="inserirPreview2" src="" class="imagemPreview">
-            </div>
-            <input type="hidden" name="numImagem2" value="2">
-        </div>
-
-        <div class="imagem-input">
-            <label>Imagem 3 (opcional):</label>
-            <input type="text" id="inserirImagem3" name="Imagem3" placeholder="Caminho da Imagem">
-            <div class="preview-div" id="inserirPreviewdiv3">
-                <label>Preview da imagem:</label>
-                <img id="inserirPreview3" src="" class="imagemPreview">
-            </div>
-            <input type="hidden" name="numImagem3" value="3">
-        </div>
-    </div>
         <input type="hidden" name="Tipo" value="Inserir">
-        </div>
-        </div>
         <div class="div-btn">
         <button type="submit">Confirmar</button> 
-        </div>
     </form>
 </div>
-
-
-<!-- form de edição de brinquedos -->
-<div id="form-container" class="formInsert">
-    <form method="POST" id="formInsert-Brinquedo" class="formInsert-Brinquedo" action="../controller/produtoProcess.php">
-    <h2>Editar Brinquedo</h2>
-    <div class="div-q-separa-socorro">
-    <div class="form-div"><!-- div q contém as inputs -->
-
-        <div class="select-input">
-            <label for="selectSelo">Selo:</label>
-            <select name="Codigo_Selo" id="codigoSelo" class="select-form" required>
-                <option value="" selected disabled hidden></option> 
-                <?php foreach ($selos as $selo) { ?>
-                    <option required name="Codigo_Selo" value="<?php echo $selo['Codigo_Selo'];?>"><?php echo $selo['Nome_Selo']; ?></option>
-                <?php } ?>
-            </select>
-        </div>
-
-        <div class="select-input">
-            <label for="selectCate">Categoria:</label>
-            <select name="Codigo_Categoria" id="codigoCate" class="select-form" required>
-                <option value="" selected disabled hidden></option>
-                <?php foreach ($categorias as $categoria) { ?>
-                    <option required name="Codigo_Categoria" value="<?php echo $categoria['Codigo_Categoria'];?>"><?php echo $categoria['Nome_Categoria']; ?></option>
-                <?php } ?>
-            </select>
-        </div>
-
-        <input type="hidden" id="codigoBrinq" name="codigoBrinq" required>
-
-        <input type="text" id="nomeBrinq" name="Nome_Brinq" placeholder="Nome" required>
-                
-        <input type="number" id="precoBrinq" step="0.01" min="0.01" name="Preco_Brinq" placeholder="Preço" required>
-        
-        <input type="number" id="notaBrinq" step="0.5" min="0" max="5" name="Nota" placeholder="Nota" required>
-        
-        <input type="text" id="fabriBrinq" name="Fabricante" placeholder="Fabricante" required>
-        
-        <input type="text" id="descBrinq" name="Descricao" placeholder="Descrição" required>
-        
-        <input type="text" id="faixaBrinq" name="Faixa_Etaria" placeholder="Faixa Etária" required>
-    </div>
-    <div class="form-div-img"> <!-- div q contém as imagens -->
-        
-        <div class="imagens-container">
-        <div class="imagem-input">
-                <label>Imagem Principal:</label>
-                <input type="text" id="imagem1" name="Imagem1" placeholder="Caminho da Imagem" required>
-
-                <div class="preview-div" id="previewdiv1">
-                <label>Preview da imagem:</label>
-                <img id="preview1" src="" class="imagemPreview">
-                </div>
-
-                
-            </div>
-            <div class="imagem-input">
-                <label>Imagem 2 (opcional):</label>
-                <input type="text" id="imagem2" name="Imagem2" placeholder="Caminho da Imagem">
-
-                <div class="preview-div" id="previewdiv2">
-                <label>Preview da imagem:</label>
-                <img id="preview2" src="" class="imagemPreview">
-                </div>
-
-                
-            </div>
-            <div class="imagem-input">
-                <label>Imagem 3 (opcional):</label>
-                <input type="text" id="imagem3" name="Imagem3" placeholder="Caminho da Imagem">
-
-                <div class="preview-div" id="previewdiv3">
-                <label>Preview da imagem:</label>
-                <img id="preview3" src="" class="imagemPreview">
-                </div>
-
-            </div>
-        </div>
-
-        <input type="hidden" id="numImagem1" name="numImagem1" value="1">
-        <input type="hidden" id="numImagem2" name="numImagem2" value="2">
-        <input type="hidden" id="numImagem3" name="numImagem3" value="3"> <!-- sim, todos esses campos hidden são absolutamente necessários -->
-        <input type="hidden" id="codigoImagem1" name="codigoImagem1">
-        <input type="hidden" id="codigoImagem2" name="codigoImagem2">
-        <input type="hidden" id="codigoImagem3" name="codigoImagem3">
-        <input type="hidden" name="Tipo" value="Atualizar">
-        </div>
-        </div>
-        <div class="div-btn">
-        <button type="submit">Atualizar</button> 
-        </div>
-    </form>
 </div>
-
+</div>
+</div>
     <?php include("footer.php") ?>
 </body>
 </html>

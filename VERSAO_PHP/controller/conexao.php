daf7a3e4f2f47719f18232a65e22eb3fa06146b1
CNSinelli <nicolassinelli1@gmail.com>
2024-11-29 00:02:49 -0300

    adicionando conexão e banco

--- VERSAO_PHP/controller/conexao.php
+++ VERSAO_PHP/controller/conexao.php
@@ -1,14 +1,13 @@
 <?php
 
-  $db_name = "GrupoCriatil";
-  $db_host = "localhost";
-  $db_user = "root";
-  $db_pass = "";
+  $db_name = 'GrupoCriatil';
+  $db_host = 'projectt.ddns.net';
+  $db_port = '3789';  
+  $db_user = 'GrupoCriatil';
+  $db_pass = 'GrupoCriatil';
 
-  $conn = new PDO("mysql:dbname=". $db_name .";host=". $db_host, $db_user, $db_pass);
+  $conn = new PDO("mysql:dbname=$db_name;host=$db_host;port=$db_port", $db_user, $db_pass);
 
-  // Habilitar erros PDO
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
 ?>
